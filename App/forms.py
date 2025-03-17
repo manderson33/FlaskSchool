c15f3f116a1b2704d8f54d177ea4790c5b01aec3
andersonmar33 <anderson.mar33@gmail.com>
2025-03-17 10:12:39 +0400

    school 03.17.2025

--- App/forms.py
+++ App/forms.py
@@ -2,7 +2,14 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
-
 class AddTaskForm(FlaskForm):
-    title = StringField('Title', validators=[DataRequired()])
-    submit = SubmitField('Submit')
+   title = StringField('Title', validators=[DataRequired()])
+   submit = SubmitField('Submit')
+
+
+class AddRectangle(FlaskForm):
+    length = StringField('Length', validators=[DataRequired()])
+    width = StringField('Width', validators=[DataRequired()])
+    area = SubmitField('Calculate Area')
+    perimeter = SubmitField('Calculate Perimeter')
+
--- App/routes.py
+++ App/routes.py
@@ -1,3 +1,4 @@
+from crypt import methods
 from datetime import datetime
 
 from flask import render_template,redirect,url_for,flash,request
@@ -10,8 +11,30 @@ import forms
 def index():
     return render_template('index.html')
 
-@app.route('/about', methods=['GET', 'POST']) # get and post
+@app.route('/about')
 def about():
+    return render_template("about.html")
+
+@app.route('/rectangle', methods=['GET', 'POST'])
+def rectangle():
+    form = forms.AddRectangle()
+    area = None
+    perimeter = None
+
+    if request.method == 'POST':
+        try:
+            length = float(form.length.data)
+            width = float(form.width.data)
+            if form.area.data:  # If 'Calculate Area' was clicked
+                area = length * width
+            if form.perimeter.data:  # If 'Calculate Perimeter' was clicked
+                perimeter = 2 * (length + width)
+        except ValueError:
+            pass  # Handle invalid input gracefully
+
+    return render_template('rectangle.html', form=form, area=area, perimeter=perimeter)
+@app.route('/form', methods=['GET', 'POST']) # get and post
+def form():
     form = forms.AddTaskForm()
     if form.validate_on_submit():
         new_task = Task(title=form.title.data, date=datetime.utcnow())  # Save task to DB
@@ -19,11 +42,11 @@ def about():
         db.session.commit()  # Commit changes
 
         flash('Task added successfully!', 'success')
-        return redirect(url_for('about'))  # Redirect to avoid form resubmission
+        return redirect(url_for('form'))  # Redirect to avoid form resubmission
 
         # Retrieve tasks from the database to display on the page
     tasks = Task.query.all()
-    return render_template('about.html', form=form, tasks=tasks)
+    return render_template('form.html', form=form, tasks=tasks)
 
 
 '''
